d88160978aa617d31e48386322505b0fc13c4075
YUJUNG <heoyujung0118@gmail.com>
2023-06-08 22:13:31 +0900

    제목 수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,16 @@
 import { PropTypes } from "prop-types";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt="coverImg"></img>
-      <h2>(새로고침)<a href="/movie">{title}</a></h2>  {/* 브라우저 새로고침 */}
-      <h2>(새로고침X)<Link to= {`/movie/${id}`}>{title}</Link></h2>  {/* 브라우저 새로고침 X */}
-      <p>{summary}</p>
+      {/* <h2>(새로고침)<a href="/movie">{title}</a></h2>  브라우저 새로고침 */}
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>{" "}
+      {/* 브라우저 새로고침 X */}
+      <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
@@ -18,7 +21,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
-  id : PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
